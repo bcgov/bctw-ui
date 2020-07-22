31765f7807c33b445db565231acd2e58f4cf3706
Garcia <Ronald.Garcia@quartech.com>
2020-07-22 15:15:01 -0700

    CI/CD implementation for UI

---
+++ openshift/settings.sh
@@ -0,0 +1,6 @@
+export PROJECT_NAMESPACE="dgsbmb"
+export GIT_URI="https://github.com/bcgov/bctw-ui.git"
+export GIT_REF="master"
+
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden with the values of GIT_URI and GIT_REF
