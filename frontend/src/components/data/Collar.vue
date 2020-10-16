b6ce28b587ae1d02a68bfbe2f50967caf58f13ce
jordan <jordan.craven@quartech.com>
2020-10-16 11:42:13 -0700

    modal

--- frontend/src/components/data/Collar.vue
+++ frontend/src/components/data/Collar.vue
@@ -1,67 +1,54 @@
 <template>
   <div >
-    <vs-table
-      pagination
-      :data="collars.availableCollars"
-      v-model="selected"
-      @selected="handleSelected"
-    >
+    <!-- iterate the collars object  -->
+    <div v-for="(value, propertyName) in collars" :key="propertyName">
+      <vs-table
+        pagination
+        :data="value"
+        v-model="selected"
+        @selected="handleSelected"
+      >
       <template slot="header">
-        <h3>Available Collars</h3>
+        <h3>{{propertyName === 'availableCollars' ? 'Available Collars' : 'Assignable Collars'}}</h3>
       </template>
 
+      <!-- iterate the properties of the first collar 
+      in each collar type to get the table headers -->
       <template slot="thead">
-        <vs-th v-for="(value, propName) in collars.availableCollars[0]" :key="propName">
-          {{propName}}
+        <vs-th v-for="(v, p) in value[0]" :key="p">
+          {{p}}
         </vs-th>
       </template>
 
+      <!-- iterate each collar types collars to display the data -->
       <template slot-scope="{data}">
-        <vs-tr :key="index" v-for="(collar, index) in collars.availableCollars" :data="collar" >
+        <vs-tr :key="index" v-for="(collar, index) in value" :data="collar" >
           <vs-td :key="v" v-for="(k, v) in collar" :data="k">
             {{k}}
           </vs-td>
         </vs-tr> 
       </template>
     </vs-table>
-    <!-- <button>See Collar Details</button> -->
     <br/>
-
-    <vs-table 
-      pagination
-      :data="collars.assignableCollars"
-      v-model="selected"
-      @selected="handleSelected"
-    >
-      <template slot="header">
-        <h3>{{title}} Assignable Collars</h3>
-      </template>
-
-      <template slot="thead">
-        <vs-th v-for="(value, propName) in collars.assignableCollars[0]" :key="propName">
-          {{propName}}
-        </vs-th>
-      </template>
-
-      <template slot-scope="{data}">
-        <vs-tr :key="index" v-for="(collar, index) in collars.assignableCollars" :data="collar" >
-          <vs-td :key="v" v-for="(k, v) in collar" :data="k">
-            {{k}}
-          </vs-td>
-        </vs-tr> 
-      </template>
-    </vs-table>
-    <!-- <pre>{{ selected['Device ID'] }}</pre> -->
+    </div> <!-- end iterate properties of $store.collars -->
     <div>
-      <button>Register New Collar</button>
-      <button>Retire Collar</button>
-      <button>See Collar Details</button>
+      <vs-button type="border">Register New Collar</vs-button>
+      <vs-button type="border">Retire Collar</vs-button>
+      <vs-button @click="handleEditClick" type="border">See Collar Details</vs-button>
     </div>
+    <edit-modal 
+      v-on:update:modal="handleEditClick" 
+      :active="showEditModal">
+    </edit-modal>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import Vue from 'vue';
+import { mapState } from 'vuex';
+import EditModal from './EditModal';
+
+Vue.component('edit-modal', EditModal);
 
 export default {
   props: {
@@ -71,13 +58,17 @@ export default {
   computed: mapState(['collars']),
   data: function() {
     return {
+      showEditModal: false,
+      showAddModal: false,
       selected: [],
-      message: 'hi',
     }
   },
   methods: {
     handleSelected(tr) {
       this.$vs.notify({ title: `device ${tr['Device ID']} selected` })
+    },
+    handleEditClick() {
+      this.showEditModal = !this.showEditModal;
     }
   },
   mounted() {
---
+++ frontend/src/components/data/EditModal.vue
@@ -0,0 +1,32 @@
+<template>
+  <vs-popup 
+    class="holamundo" 
+    title="Lorem ipsum dolor sit amet"
+    :active.sync="shouldShow">
+    <p>
+      Lorem ipsum dolor sit amet.
+    </p>
+  </vs-popup>
+</template>
+  
+<script>
+export default {
+  name: 'EditModal',
+  props: {
+    active: Boolean
+  },
+  computed: {
+    shouldShow: {
+      get: function() {
+        return this.active;
+      },
+      set: function(newValue) {
+        this.$emit('update:modal', newValue)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
