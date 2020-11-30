70b1a09ec0ff02bcbb9734299a4b3482ad93d917
jordan <jordan.craven@quartech.com>
2020-11-30 15:50:48 -0800

    fix9000

--- backend/js/server.js
+++ backend/js/server.js
@@ -61,7 +61,7 @@ const appendQueryToUrl = (url, query) => {
     url += `?${query}`;
 };
 
-const proxyApi = async function (req, res, next) {
+const proxyApi = function (req, res, next) {
   const endpoint = req.params.endpoint; // The url
 
   // The parameter string
@@ -89,21 +89,16 @@ const proxyApi = async function (req, res, next) {
 
   if (req.method === 'POST') {
     console.log(`post request detected: body: ${JSON.stringify(req.body)}`);
-    const response = await fetch(url, {method: 'POST', body: req.body });
-    if (response.ok) {
-      response.json.then((data) => {
-        res.json(data)
-      });
-    } else {
-      res.status(500).json(response.text())
-    }
-    // needle('post', url, req.body, (err, _, body) => {
-    //   if (err) {
-    //     console.log(`error communicating with API on post request: ${err}`);
-    //     return res.status(500).json({error: err});
-    //   }
-    //   res.json(body);
-    // });
+    fetch(url, {method: 'POST', body: req.body })
+      .then(response => {
+        if (response.ok) {
+          response.json().then((data) => {
+            res.json(data)
+          });
+        } else {
+          res.status(500).json(response.text())
+        }
+      }).catch(e => res.status(500).json({error: e}));
   } else {
     needle(url,(err,_,body) => {
       if (err) {
