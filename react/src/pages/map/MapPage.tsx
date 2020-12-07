c089b2173be7296df4f924323a1c3d117379973d
Jamie Popkin <popkinj@littleearth.ca>
2020-12-07 14:04:50 -0800

    Leaflet is working

--- react/src/pages/map/MapPage.tsx
+++ react/src/pages/map/MapPage.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import './MapPage.css';
 
 type IMapPageProps = { }
 
 const MapPage: React.FC<IMapPageProps> = (props) => {
+  const mapRef = useRef(null);
+
+  const initMap = () => {
+    mapRef.current = L.map('map', {zoomControl:false})
+      .setView([55, -128], 10);
+
+  };
+
+  useEffect(() => {
+    initMap();
+  });
+
   return (
-    <div>map goes here</div>
+    <div id='map'></div>
   )
 }
 
