97646cd0be7f370a18229425f4319bf5f824d5ab
Jamie Popkin <popkinj@littleearth.ca>
2020-12-08 09:02:12 -0800

    Cesium half running

--- react/src/pages/terrain/TerrainPage.tsx
+++ react/src/pages/terrain/TerrainPage.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import * as Cesium from 'cesium'
+
+// Because Cesium isn't very React friendly
+// @ts-ignore
+window.CESIUM_BASE_URL = '';
 
 type ITerrainPageProps = { }
 
 const TerrainPage: React.FC<ITerrainPageProps> = (props) => {
+  const mapRef = useRef(null);
+
+  const initMap = () => {
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNzk1ZDk0My02MjJmLTQ4ZDgtYmJkMy1kY2Y0YTI0MTg3N2YiLCJpZCI6ODA1Niwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1MTEyOTM2Nn0.UoAFRv2ErXndRVOI1lltlyWFzaSpE__lpGxe4kb4GSM'
+    const extent = Cesium.Rectangle.fromDegrees(-130,45,-118,60);
+    Cesium.Camera.DEFAULT_VIEW_RECTANGLE = extent;
+    Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
+
+    mapRef.current = new Cesium.Viewer('terrain', {
+      timeline: false,
+      geocoder: false,
+      fullscreenButton: false,
+      vrButton: false,
+      homeButton: false,
+      animation: false,
+      scene3DOnly: true,
+      terrainProvider: Cesium.createWorldTerrain({
+        requestWaterMask: true,
+        requestVertexNormals: true
+      })
+    });
+  };
+
+  useEffect(() => {
+    initMap();
+  });
+
   return (
-    <div>terrain goes here</div>
+    <div id='terrain'></div>
   )
 }
 
