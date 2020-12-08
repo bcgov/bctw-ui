ebc049b4c59aee6b510d6b325d6fa170e8624f7a
Jamie Popkin <popkinj@littleearth.ca>
2020-12-08 14:49:05 -0800

    Cesium sorta drawing

--- react/src/pages/terrain/TerrainPage.tsx
+++ react/src/pages/terrain/TerrainPage.tsx
@@ -1,9 +1,12 @@
 import React, {useEffect, useRef} from 'react';
-import * as Cesium from 'cesium'
+import {Viewer, createWorldTerrain, Cartesian3, Math} from 'cesium';
+// import * as Cesium from 'cesium'
+// import 'cesium/Build/Cesium/Widgets/widgets.css'
 
 // Because Cesium isn't very React friendly
 // @ts-ignore
-window.CESIUM_BASE_URL = '';
+// window.CESIUM_BASE_URL = 'http://localhost:3000';
+// window.CESIUM_BASE_URL = 'http://localhost:3000';
 
 type ITerrainPageProps = { }
 
@@ -11,12 +14,15 @@ const TerrainPage: React.FC<ITerrainPageProps> = (props) => {
   const mapRef = useRef(null);
 
   const initMap = () => {
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNzk1ZDk0My02MjJmLTQ4ZDgtYmJkMy1kY2Y0YTI0MTg3N2YiLCJpZCI6ODA1Niwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1MTEyOTM2Nn0.UoAFRv2ErXndRVOI1lltlyWFzaSpE__lpGxe4kb4GSM'
-    const extent = Cesium.Rectangle.fromDegrees(-130,45,-118,60);
-    Cesium.Camera.DEFAULT_VIEW_RECTANGLE = extent;
-    Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
 
-    mapRef.current = new Cesium.Viewer('terrain', {
+    console.log(Viewer);
+    // const viewer = new Cesium.Viewer(document.getElementById('terrain'));
+    // Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNzk1ZDk0My02MjJmLTQ4ZDgtYmJkMy1kY2Y0YTI0MTg3N2YiLCJpZCI6ODA1Niwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1MTEyOTM2Nn0.UoAFRv2ErXndRVOI1lltlyWFzaSpE__lpGxe4kb4GSM'
+    // const extent = Cesium.Rectangle.fromDegrees(-130,45,-118,60);
+    // Cesium.Camera.DEFAULT_VIEW_RECTANGLE = extent;
+    // Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
+
+    mapRef.current = new Viewer('terrain', {
       timeline: false,
       geocoder: false,
       fullscreenButton: false,
@@ -24,10 +30,19 @@ const TerrainPage: React.FC<ITerrainPageProps> = (props) => {
       homeButton: false,
       animation: false,
       scene3DOnly: true,
-      terrainProvider: Cesium.createWorldTerrain({
+      terrainProvider: createWorldTerrain({
         requestWaterMask: true,
         requestVertexNormals: true
       })
+    });
+
+    mapRef.current.camera.setView({
+      destination: Cartesian3.fromDegrees(-125,47,400000),
+      orientation: {
+        heading: Math.toRadians(0),
+        pitch: Math.toRadians(-40),
+        roll: 0.0
+      }
     });
   };
 
