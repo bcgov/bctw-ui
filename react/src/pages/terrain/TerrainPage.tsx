451d2bc4170cc0b3b0fb07f180afdd362c3b0d98
Jamie Popkin <popkinj@littleearth.ca>
2020-12-08 15:16:24 -0800

    mostly working

--- react/src/pages/terrain/TerrainPage.tsx
+++ react/src/pages/terrain/TerrainPage.tsx
@@ -1,30 +1,20 @@
 import React, {useEffect, useRef} from 'react';
-import {Viewer, createWorldTerrain, Cartesian3, Math} from 'cesium';
-// import * as Cesium from 'cesium'
-// import 'cesium/Build/Cesium/Widgets/widgets.css'
-
-// Because Cesium isn't very React friendly
-// @ts-ignore
-// window.CESIUM_BASE_URL = 'http://localhost:3000';
-// window.CESIUM_BASE_URL = 'http://localhost:3000';
+import {Viewer, createWorldTerrain, Cartesian3, Math, Ion} from 'cesium';
+import './TerrainPage.css';
 
 type ITerrainPageProps = { }
 
 const TerrainPage: React.FC<ITerrainPageProps> = (props) => {
-  const mapRef = useRef<Cesium.Viewer>(null);
+  const mapRef = useRef(null);
 
   const initMap = () => {
 
-    console.log(Viewer);
-    // const viewer = new Cesium.Viewer(document.getElementById('terrain'));
-    // Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNzk1ZDk0My02MjJmLTQ4ZDgtYmJkMy1kY2Y0YTI0MTg3N2YiLCJpZCI6ODA1Niwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1MTEyOTM2Nn0.UoAFRv2ErXndRVOI1lltlyWFzaSpE__lpGxe4kb4GSM'
-    // const extent = Cesium.Rectangle.fromDegrees(-130,45,-118,60);
-    // Cesium.Camera.DEFAULT_VIEW_RECTANGLE = extent;
-    // Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
+    Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNzk1ZDk0My02MjJmLTQ4ZDgtYmJkMy1kY2Y0YTI0MTg3N2YiLCJpZCI6ODA1Niwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1MTEyOTM2Nn0.UoAFRv2ErXndRVOI1lltlyWFzaSpE__lpGxe4kb4GSM';
 
     mapRef.current = new Viewer('terrain', {
       timeline: false,
       geocoder: false,
+      baseLayerPicker: true,
       fullscreenButton: false,
       vrButton: false,
       homeButton: false,
