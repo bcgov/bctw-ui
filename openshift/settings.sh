b219ac0f303795a4d796626ba04bc0fc1e7bdaee
jordan <jordan.craven@quartech.com>
2021-01-04 12:37:54 -0800

    Revert "run ui pipeline on specific commit"
    This reverts commit 574a763290933ff59d6369f68c33d018e1483ae6.

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,7 +1,6 @@
 export PROJECT_NAMESPACE="dgsbmb"
 export GIT_URI="https://github.com/bcgov/bctw-ui.git"
-export GIT_REF="2ded8930c1bb122c0154fe1bce2afc6a739f9a7e"
-# export GIT_REF="master"
+export GIT_REF="master"
 
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden with the values of GIT_URI and GIT_REF
