914bba6020e5cbc3431804be59f3dd088a847046
Jamie Popkin <popkinj@littleearth.ca>
2021-01-05 10:28:31 -0800

    Leaflet drawing with basemap

--- react/src/pages/map/MapPage.tsx
+++ react/src/pages/map/MapPage.tsx
@@ -12,7 +12,22 @@ const MapPage: React.FC = () => {
     mapRef.current = L.map('map', {zoomControl:false})
       .setView([55, -128], 10);
 
+    const layerPicker = L.control.layers();
+
+    const bingOrtho = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">ESRI Basemap</a> ',
+      maxZoom: 18
+    }).addTo(mapRef.current);
+
+    const bcGovBaseLayer = L.tileLayer('https://maps.gov.bc.ca/arcgis/rest/services/province/roads_wm/MapServer/tile/{z}/{y}/{x}', { maxZoom: 24 }).addTo(mapRef.current);
+
+    layerPicker.addBaseLayer(bingOrtho, 'Bing Satellite');
+    layerPicker.addBaseLayer(bcGovBaseLayer, 'Bing Satellite');
+
+    mapRef.current.addControl(layerPicker);
+
   };
+
 
   useEffect(() => {
     initMap();
