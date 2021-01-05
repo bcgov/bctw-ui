71dbfaa29537fa2e47aafae4daadd6f86819db52
Jamie Popkin <popkinj@littleearth.ca>
2021-01-05 14:48:05 -0800

    The drawing tools

--- react/src/pages/map/MapPage.tsx
+++ react/src/pages/map/MapPage.tsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useRef} from 'react';
 import * as L from 'leaflet';
+import 'leaflet-draw';
+
 import 'leaflet/dist/leaflet.css';
 import './MapPage.css';
 
@@ -15,16 +17,39 @@ const MapPage: React.FC = () => {
     const layerPicker = L.control.layers();
 
     const bingOrtho = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">ESRI Basemap</a> ',
-      maxZoom: 18
+      attribution: '&copy; <a href="https://esri.com">ESRI Basemap</a> ',
+      maxZoom: 24
     }).addTo(mapRef.current);
 
-    const bcGovBaseLayer = L.tileLayer('https://maps.gov.bc.ca/arcgis/rest/services/province/roads_wm/MapServer/tile/{z}/{y}/{x}', { maxZoom: 24 }).addTo(mapRef.current);
+    const bcGovBaseLayer = L.tileLayer('https://maps.gov.bc.ca/arcgis/rest/services/province/roads_wm/MapServer/tile/{z}/{y}/{x}', {
+      maxZoom: 24,
+      attribution: '&copy; <a href="https://www2.gov.bc.ca/gov/content/home">BC Government</a> ',
+    });
 
     layerPicker.addBaseLayer(bingOrtho, 'Bing Satellite');
-    layerPicker.addBaseLayer(bcGovBaseLayer, 'Bing Satellite');
+    layerPicker.addBaseLayer(bcGovBaseLayer, 'BC Government');
 
     mapRef.current.addControl(layerPicker);
+
+    const drawnItems = new L.FeatureGroup();
+    mapRef.current.addLayer(drawnItems);
+
+    const drawControl = new L.Control.Draw({
+      position: 'topright',
+      draw:{
+        marker: false,
+        polyline: false,
+        circle: false,
+        circlemarker: false
+      },
+      edit: {
+
+        featureGroup: drawnItems,
+        remove: true,
+        edit: true
+      }
+      }
+    );
 
   };
 
