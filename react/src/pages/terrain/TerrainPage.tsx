19b164eb3763e44b98acf878f63f9e61470dd391
Jamie Popkin <popkinj@littleearth.ca>
2021-01-10 12:32:11 -0800

    Terrain ready for layers

--- react/src/pages/terrain/TerrainPage.tsx
+++ react/src/pages/terrain/TerrainPage.tsx
@@ -1,17 +1,28 @@
-import React, {useEffect, useRef} from 'react';
-import {Viewer, createWorldTerrain, Cartesian3, Math, Ion} from 'cesium';
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  Viewer,
+  createWorldTerrain,
+  Cartesian3,
+  Math,
+  Ion,
+  GeoJsonDataSource
+} from 'cesium';
 import './TerrainPage.css';
 
 
 const TerrainPage: React.FC = () => {
   const mapRef = useRef(null);
 
+  // const [tracks,setTracks] = useState(new L.GeoJSON());
+
+  const [pings,setPings] = useState(new GeoJsonDataSource('pings'));
+
   const initMap = (): void => {
 
     Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNzk1ZDk0My02MjJmLTQ4ZDgtYmJkMy1kY2Y0YTI0MTg3N2YiLCJpZCI6ODA1Niwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1MTEyOTM2Nn0.UoAFRv2ErXndRVOI1lltlyWFzaSpE__lpGxe4kb4GSM';
 
     mapRef.current = new Viewer('terrain', {
-      timeline: false,
+      timeline: true,
       geocoder: false,
       baseLayerPicker: true,
       fullscreenButton: false,
@@ -35,13 +46,21 @@ const TerrainPage: React.FC = () => {
     });
   };
 
+  const createPingLayer = (pings) => {
+    console.log(pings)
+  };
+
   useEffect(() => {
     initMap();
   });
 
+  useEffect(() => {
+    createPingLayer(pings);
+  },[pings]);
+
   return (
     <div id='terrain'></div>
   )
-}
+};
 
 export default TerrainPage;
