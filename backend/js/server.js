e1b848dba68d1d8bdfb456c78f940bac9bd55d30
Jamie Popkin <popkinj@littleearth.ca>
2021-06-10 12:29:51 -0700

    Bypass shield when developing

--- backend/js/server.js
+++ backend/js/server.js
@@ -225,6 +225,45 @@ const authenticate = function (req, res, next) {
 };
 
 /**
+ * # onboardingRedirect
+ * If you get here you have a valid IDIR.
+ * Check if the user is registerd in the database.
+ * If yes.... Pass through.
+ * Else... Direct to the onboarding page.
+ * @param req {object} Express request object
+ * @param res {object} Express response object
+ * @param next {function} Express function to continue on
+ */
+const onboardingRedirect = async (req,res,next) => {
+  // Collect all user data from the keycloak object
+  const data = req.kauth.grant.access_token.content;
+  const domain = data.preferred_username.split('@')[1];
+  const user = data.preferred_username.split('@')[0];
+  const email = data.email;
+  const givenName = data.given_name;
+  const familyName = data.family_name;
+
+  // Get a list of all allowed users
+  const sql = 'select idir from bctw.user'
+  const client = await pgPool.connect();
+  const result = await client.query(sql);
+  const idirs = result.rows.map((row) => row.idir);
+  // Is the current user registered: Boolean
+  const registered = (idirs.includes(user)) ? true : false;
+
+  // Formulate the url and data to be sent to the onboarding page
+  let url = `/onboarding?user=${user}&domain=${domain}&email=${email}`;
+  url += `&given=${givenName}&family=${familyName}`;
+
+  if (registered) {
+    next(); // pass through
+  } else {
+    res.redirect(url); // reject and go to the onboarding page
+  }
+  client.release(); // Release database connection
+};
+
+/**
  * # onboarding
  * Accept requests for access to the site.
  * Send requests to product owner via email (CHES).
@@ -264,44 +303,6 @@ const onboardingAccess = async (req,res) => {
     }
   });
 
-/**
- * # onboardingRedirect
- * If you get here you have a valid IDIR.
- * Check if the user is registerd in the database.
- * If yes.... Pass through.
- * Else... Direct to the onboarding page.
- * @param req {object} Express request object
- * @param res {object} Express response object
- * @param next {function} Express function to continue on
- */
-  const onboardingRedirect = async (req,res,next) => {
-    // Collect all user data from the keycloak object
-    const data = req.kauth.grant.access_token.content;
-    const domain = data.preferred_username.split('@')[1];
-    const user = data.preferred_username.split('@')[0];
-    const email = data.email;
-    const givenName = data.given_name;
-    const familyName = data.family_name;
-
-    // Get a list of all allowed users
-    const sql = 'select idir from bctw.user'
-    const client = await pgPool.connect();
-    const result = await client.query(sql);
-    const idirs = result.rows.map((row) => row.idir);
-    // Is the current user registered: Boolean
-    const registered = (idirs.includes(user)) ? true : false;
-
-    // Formulate the url and data to be sent to the onboarding page
-    let url = `/onboarding?user=${user}&domain=${domain}&email=${email}`;
-    url += `&given=${givenName}&family=${familyName}`;
-
-    if (registered) {
-      next(); // pass through
-    } else {
-      res.redirect(url); // reject and go to the onboarding page
-    }
-    client.release(); // Release database connection
-  };
 
   const pretoken = tokenParcel.data.access_token;
   if (!pretoken) return res.status(500).send('Authentication failed');
@@ -363,10 +364,19 @@ var app = express()
   .use(expressSession(session))
   .use(keycloak.middleware())
   .use(gardenGate) // Keycloak Gate
-  .get('/onboarding', keycloak.protect(), onboarding)
-  .post('/onboarding', keycloak.protect(), onboardingAccess)
-  .all('*', keycloak.protect(), onboardingRedirect)
   .get('/denied', denied);
+
+if (isProd) {
+  app
+    .get('/onboarding', keycloak.protect(), onboarding)
+    .post('/onboarding', keycloak.protect(), onboardingAccess)
+    .all('*', keycloak.protect(), onboardingRedirect);
+} else{
+  app
+    .get('/onboarding',  onboarding)
+    .post('/onboarding', onboardingAccess)
+    .all('*', onboardingRedirect);
+}
 
 if (isTest) {
   app
