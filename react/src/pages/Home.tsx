b796054c3275152b0bcf05af06a32982758f9ade
Victor Chong <62109882+victorchong@users.noreply.github.com>
2021-06-24 15:39:32 -0700

    Fixed broken link

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -10,7 +10,7 @@ const Home = (): JSX.Element => {
         Welcome to the BC Telemetry Warehouse (BCTW). The BCTW is an application and database to manage and store the Province of British Columbia’s wildlife telemetry observations to support informed management decisions and improving conservation efforts.
       </Typography>
       <Typography paragraph>
-      Search for <a href='www.gov.bc.ca/wildlife-species-information'>wildlife data and information</a>
+      Search for <a href='https://www.gov.bc.ca/wildlife-species-information'>wildlife data and information</a>
       </Typography>
       <Typography paragraph>
         Use the <a href='https://catalogue.data.gov.bc.ca/dataset?q=wsi&download_audience=Public&sort=score+desc%2C+record_publish_date+desc'>DataBC Data Catalogue</a> to access publicly available telemetry data layers derived from the BC Telemetry Warehouse and Wildlife Species Inventory (SPI) database for desktop GIS analyses.
