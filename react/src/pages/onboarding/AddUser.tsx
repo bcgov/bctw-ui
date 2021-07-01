cd5e18df3a76aeeebb7c5a4859a724f373dee174
Jamie Popkin <popkinj@littleearth.ca>
2021-07-01 10:49:04 -0700

    Documentation

--- react/src/pages/onboarding/AddUser.tsx
+++ react/src/pages/onboarding/AddUser.tsx
@@ -3,6 +3,14 @@ import PendingUser from 'components/onboarding/Pending';
 import DeniedUser from 'components/onboarding/Denied';
 import './AddUser.css';
 
+/**
+ * # AddUser
+ * This page displays one of three things depending on
+ * the status of the IDIR/BCeID user
+ * 1. Application for access form
+ * 2. Pending access approval/denial
+ * 3. Access denied
+ */
 const AddUser = (): JSX.Element => {
   return (
     <div>
