62b91ce7d34237d6c202f881561e06d845d92e00
Jamie Popkin <popkinj@littleearth.ca>
2021-07-29 09:52:06 -0700

    Better re-routing to index.html

--- backend/js/server.js
+++ backend/js/server.js
@@ -363,6 +363,15 @@ const denied = function (req, res) {
   res.render('denied', req);
 }
 
+/* ## devServerRedirect
+  Redirect traffic to the React dev server 
+  @param req {object} Node/Express request object
+  @param res {object} Node/Express response object
+*/
+const devServerRedirect = function (req, res) {
+  res.redirect('locahost:1111');
+}
+
 // use enhanced logging in non-production environments
 const logger = isProd ? 'combined' : 'dev';
 
@@ -414,10 +423,16 @@ if (isTest) {
     .get('/', pageHandler)
 }
 
-// Remaining server configuration
+// Static assets 
 app
-  .use(express['static'](path.join(__dirname, '../../react/build')))
-  // .get('*', notFound);
+  .use(express['static'](path.join(__dirname, '../../react/build')));
+
+// All remaining requests go to React
+if (isProd) {
+  app.get('*', __dirname + '../../build/index.html');
+} else {
+  app.get('*', devServerRedirect);
+}
 
 // Start server
 http.createServer(app).listen(8080);
