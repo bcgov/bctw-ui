2c65ddcd24071c387176b6c6a9d55a36b714c9ef
Victor Chong <62109882+victorchong@users.noreply.github.com>
2021-07-30 14:21:57 -0700

    Add Help button & text to full-screen dialogs

--- react/src/components/modal/DialogFullScreen.tsx
+++ react/src/components/modal/DialogFullScreen.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Dialog from '@material-ui/core/Dialog';
 import Fade from '@material-ui/core/Fade';
-import Slide from '@material-ui/core/Slide';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Theme } from '@material-ui/core/styles/createTheme';
 import { TransitionProps } from '@material-ui/core/transitions';
 import { ModalProps } from 'components/component_interfaces';
-import { IconButton } from '@material-ui/core';
-import { Icon } from 'components/common';
 import './modal.scss';
 
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowBack from '@material-ui/icons/ArrowBack';
+import HelpIcon from '@material-ui/icons/Help';
 
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import { classicNameResolver } from 'typescript';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  fsDialogBackBtn: {
-    color: '#ffffff'
-  },
   fsDialogHeader: {
-    height: '70px'
+    height: '70px',
+  },
+  fsDialogBackBtn: {
+    marginRight: 'auto',
+    color: '#ffffff',
+    float: 'left'
+  },
+  fsDialogHelpBtn: {
+    color: '#ffffff',
+    float: 'right'
   }
 }));
 
@@ -56,7 +58,17 @@ export default function FullScreenDialog({ open, handleClose, children }: ModalP
               disableElevation
               startIcon={<ArrowBack />}
               onClick={(e): void => handleClose(false)}>
-              Cancel and Exit
+                Cancel and Exit
+            </Button>
+            <Button
+              className={classes.fsDialogHelpBtn}
+              size="large"
+              variant="text"
+              color="primary"
+              disableElevation
+              startIcon={<HelpIcon />}
+              onClick={(e): void => {window.open('https://apps.nrs.gov.bc.ca/int/confluence/display/BCTW/')}}>
+                Help
             </Button>
           </Toolbar>
         </Container>
