d0dda37634bc563d38cbc834fbf19c1361132869
Victor Chong <62109882+victorchong@users.noreply.github.com>
2021-08-03 10:58:22 -0700

    Switched from HashRouter to to BrowserRouter and commented out Logout page handler

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,22 +1,20 @@
 import 'styles/AppLayout.scss';
 import 'styles/Data.scss';
 
-import { makeStyles, ThemeProvider } from '@material-ui/core';
+import AppFooter from 'components/common/AppFooter';
 import AppHeader from 'components/common/AppHeader';
-import { ResponseProvider } from 'contexts/ApiResponseContext';
-import { UserContext, UserStateContextProvider } from 'contexts/UserContext';
-import { AlertContext, AlertStateContextProvider } from 'contexts/UserAlertContext';
-import { QueryClient, QueryClientProvider } from 'react-query';
-// import { ReactQueryDevtools } from 'react-query/devtools'
-import { HashRouter } from 'react-router-dom';
-import {} from 'react-router-dom';
-
+import DefaultLayout from 'pages/layouts/DefaultLayout';
 import appTheme from 'themes/appTheme';
 
+import { AlertContext, AlertStateContextProvider } from 'contexts/UserAlertContext';
 import { AppRouter } from './AppRouter';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ResponseProvider } from 'contexts/ApiResponseContext';
 import { SnackbarWrapper } from 'components/common';
-import DefaultLayout from 'pages/layouts/DefaultLayout';
-import AppFooter from 'components/common/AppFooter';
+import { UserContext, UserStateContextProvider } from 'contexts/UserContext';
+import { makeStyles, ThemeProvider } from '@material-ui/core';
+// import { ReactQueryDevtools } from 'react-query/devtools'
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -46,7 +44,7 @@ export default function App(): JSX.Element {
                     <UserContext.Consumer>
                       {(): React.ReactNode => {
                         return (
-                          <HashRouter>
+                          <BrowserRouter>
                             <div className={classes.root}>
                               <AppHeader />
                               <div className={'app-body'}>
@@ -60,7 +58,7 @@ export default function App(): JSX.Element {
                               </div>
                               <AppFooter />
                             </div>
-                          </HashRouter>
+                          </BrowserRouter>
                         );
                       }}
                     </UserContext.Consumer>
--- react/src/AppRouter.tsx
+++ react/src/AppRouter.tsx
@@ -1,20 +1,21 @@
-import CritterPage from 'pages/data/animals/CritterPage';
+import AddUser from 'pages/onboarding/AddUser'
+import AdminHandleRequestPermissionPage from 'pages/permissions/AdminHandleRequestPermission';
 import CodePage from 'pages/data/codes/CodePage';
 import CollarPage from 'pages/data/collars/CollarPage';
+import CritterPage from 'pages/data/animals/CritterPage';
 import Home from 'pages/Home';
-import AddUser from 'pages/onboarding/AddUser'
-import MapPage from 'pages/map/MapPage';
 import Import from 'pages/data/bulk/Import';
-// import TerrainPage from 'pages/terrain/TerrainPage';
+// import Logout from 'pages/Logout';
+import MapPage from 'pages/map/MapPage';
 import GrantCritterAccessPage from 'pages/permissions/GrantCritterAccessPage';
+// import TerrainPage from 'pages/terrain/TerrainPage';
+import OwnerRequestPermission from 'pages/permissions/OwnerRequestPermission';
+import UserAdminPage from 'pages/admin/UserAdmin';
 import UserProfile from 'pages/user/UserProfile';
+
 import { FunctionComponent } from 'react';
 import { Redirect, Route, Switch, BrowserRouter } from 'react-router-dom';
 // import { useHistory } from 'react-router-dom';
-import UserAdminPage from 'pages/admin/UserAdmin';
-import OwnerRequestPermission from 'pages/permissions/OwnerRequestPermission';
-import AdminHandleRequestPermissionPage from 'pages/permissions/AdminHandleRequestPermission';
-import Logout from 'pages/Logout';
 
 export type RouteKey = {
   path: string;
@@ -27,21 +28,21 @@ export type RouteKey = {
 };
 
 const AppRoutes: RouteKey[] = [
-  { name: 'home', path: '/home', title: 'Home', component: Home, sort: 0, icon: 'home' },
-  { name: 'onboarding', path: '/onboarding', title: 'onboarding', component: AddUser, sort: 101 },
-  { name: 'map', path: '/map', title: 'Map', component: MapPage, sort: 1, icon: 'map' },
-  // { name: 'terrain', path: '/terrain', title: 'Terrain Viewer', component: TerrainPage, sort: 1, icon: 'terrain' },
   { name: 'animals', path: '/animals', title: 'Animals', component: CritterPage, sort: 2, icon: 'critter' },
-  { name: 'devices', path: '/devices', title: 'Devices', component: CollarPage, sort: 3, icon: 'collar' },
-  { name: 'import', path: '/import', title: 'Bulk Import', component: Import, sort: 4, icon: 'arrow-up'},
-  { name: 'profile', path: '/profile', title: 'My Profile', component: UserProfile, sort: 5, icon: 'profile' },
-  { name: 'owner-access', path: '/owner-access', title: 'Delegation', component: OwnerRequestPermission, sort: 6, icon: 'person' },
-  { name: 'handle-permission-request', path: '/handle-permission-request', title: 'Requests', component:AdminHandleRequestPermissionPage, sort: 7, icon: 'edit' },
   { name: 'animal-access', path: '/animal-access', title: 'Animal Access', component: GrantCritterAccessPage, sort: 8, icon: 'filter' },
-  { name: 'user-admin', path: '/user-admin', title: 'Users', component: UserAdminPage, sort: 9, icon: 'admin' },
   { name: 'codes', path: '/codes', title: 'Code Tables', component: CodePage, sort: 10, icon: 'code' },
-  { name: 'logout', path: '/logout', title: 'Logout', component: Logout, sort: 99 },
+  { name: 'devices', path: '/devices', title: 'Devices', component: CollarPage, sort: 3, icon: 'collar' },
+  { name: 'home', path: '/home', title: 'Home', component: Home, sort: 0, icon: 'home' },
+  { name: 'handle-permission-request', path: '/handle-permission-request', title: 'Requests', component: AdminHandleRequestPermissionPage, sort: 7, icon: 'edit' },
+  { name: 'import', path: '/import', title: 'Bulk Import', component: Import, sort: 4, icon: 'arrow-up' },
+  // { name: 'logout', path: '/logout', title: 'Logout', component: Logout, sort: 99 },
+  { name: 'onboarding', path: '/onboarding', title: 'onboarding', component: AddUser, sort: 101 },
+  { name: 'owner-access', path: '/owner-access', title: 'Delegation', component: OwnerRequestPermission, sort: 6, icon: 'person' },
+  { name: 'map', path: '/map', title: 'Map', component: MapPage, sort: 1, icon: 'map' },
   { name: 'notFound', path: '/*', title: 'Not Found', component: (): JSX.Element => <div>page not found!</div>, sort: 100 },
+  { name: 'profile', path: '/profile', title: 'My Profile', component: UserProfile, sort: 5, icon: 'profile' },
+  // { name: 'terrain', path: '/terrain', title: 'Terrain Viewer', component: TerrainPage, sort: 1, icon: 'terrain' },
+  { name: 'user-admin', path: '/user-admin', title: 'Users', component: UserAdminPage, sort: 9, icon: 'admin' }
 ];
 
 const AppRouter = (): JSX.Element => {
