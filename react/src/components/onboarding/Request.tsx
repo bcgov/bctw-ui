f0f584add8c59ebf2e66a75c9471329fb0819d26
Victor Chong <62109882+victorchong@users.noreply.github.com>
2021-08-06 18:51:04 -0700

    Update UX for onboarding page

--- react/src/components/onboarding/Request.tsx
+++ react/src/components/onboarding/Request.tsx
@@ -1,3 +1,5 @@
+import 'styles/form.scss';
+
 import { Box, Grid } from '@material-ui/core';
 import { Button, FormControl, InputLabel, NativeSelect, OutlinedInput, TextField, TextareaAutosize } from "@material-ui/core";
 import { createUrl } from 'api/api_helpers';
@@ -17,16 +19,16 @@ const RequestUser = (): JSX.Element => {
   /**
    * Here is all our form state.
    */
-   const [accessType, setAccessType] = useState('');
-   const [populationUnit, setPopulationUnit] = useState('');
-   const [projectManager, setProjectManager] = useState('');
-   const [projectName, setProjectName] = useState('');
-   const [projectRole, setProjectRole] = useState('');
-   const [reason, setReason] = useState('');
-   const [region, setRegion] = useState('');
-   const [textMessageNumber, setTextMessageNumber] = useState('');
-   const [species, setSpecies] = useState('');
- 
+  const [accessType, setAccessType] = useState('');
+  const [populationUnit, setPopulationUnit] = useState('');
+  const [projectManager, setProjectManager] = useState('');
+  const [projectName, setProjectName] = useState('');
+  const [projectRole, setProjectRole] = useState('');
+  const [reason, setReason] = useState('');
+  const [region, setRegion] = useState('');
+  const [species, setSpecies] = useState('');
+  const [textMessageNumber, setTextMessageNumber] = useState('');
+
   /**
    * ## submitForm
    * Form payload and submit.
@@ -40,8 +42,8 @@ const RequestUser = (): JSX.Element => {
       projectRole,
       reason,
       region,
-      textMessageNumber,
-      species
+      species,
+      textMessageNumber
     }
     const url = createUrl({ api: 'onboarding' });
 
@@ -78,45 +80,43 @@ const RequestUser = (): JSX.Element => {
       <Box className="fieldset-form">
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <div>
-              <TextField label='Project Name' onChange={(e) => { setProjectName(e.target.value) }} size='small' variant={'outlined'} />
-            </div>
-            <div>
-              <TextField label='Role in Project' onChange={(e) => { setProjectRole(e.target.value) }} size='small' variant={'outlined'} />
-            </div>
-            <div>
-              <TextField label='Project Manager' onChange={(e) => { setProjectManager(e.target.value) }} size='small' variant={'outlined'} />
-            </div>
-            <div>
-              <TextField label='Target Species' onChange={(e) => { setSpecies(e.target.value) }} size='small' variant={'outlined'} />
-            </div>
-            <div>
-              <TextField label='Region' onChange={(e) => { setRegion(e.target.value) }} size='small' variant={'outlined'} />
-            </div>
-            <div>
-              <TextField label='Population Unit Name' onChange={(e) => { setPopulationUnit(e.target.value) }} size='small' variant={'outlined'} />
-            </div>
-            <div>
+            <TextField label='Project Name' onChange={(e) => { setProjectName(e.target.value) }} size='small' variant={'outlined'} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Role in Project' onChange={(e) => { setProjectRole(e.target.value) }} size='small' variant={'outlined'} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Project Manager' onChange={(e) => { setProjectManager(e.target.value) }} size='small' variant={'outlined'} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Target Species' onChange={(e) => { setSpecies(e.target.value) }} size='small' variant={'outlined'} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Region' onChange={(e) => { setRegion(e.target.value) }} size='small' variant={'outlined'} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Population Unit Name' onChange={(e) => { setPopulationUnit(e.target.value) }} size='small' variant={'outlined'} />
+          </Grid>
+          <Grid item xs={12}>
             <FormControl className={'select-control'} size='small' variant={'outlined'}>
-                <InputLabel>Target Level of Access</InputLabel>
-                <NativeSelect onChange={(e) => { setAccessType(e.target.value) }} value={accessType} variant={'outlined'}>
-                  <option value={''}></option>
-                  <option value={'Administrator'}>Administrator</option>
-                  <option value={'Manager'}>Manager</option>
-                  <option value={'Editor'}>Editor</option>
-                  <option value={'Observer'}>Observer</option>
-                </NativeSelect>
-              </FormControl>
-            </div>
-            <div>
-              <TextareaAutosize placeholder="Describe the reason for wanting access to this application" onChange={(e) => { setReason(e.target.value) }} />
-            </div>
-            <div>
-              <TextField label='Mobile Number to Receive Text Messages' onChange={(e) => { setTextMessageNumber(e.target.value) }} />
-            </div>
-            <div>
+              <InputLabel>Target Level of Access</InputLabel>
+              <NativeSelect onChange={(e) => { setAccessType(e.target.value) }} value={accessType} variant={'outlined'}>
+                <option value={''}></option>
+                <option value={'Administrator'}>Administrator</option>
+                <option value={'Manager'}>Manager</option>
+                <option value={'Editor'}>Editor</option>
+                <option value={'Observer'}>Observer</option>
+              </NativeSelect>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <TextareaAutosize placeholder="Describe the reason for wanting access to this application" onChange={(e) => { setReason(e.target.value) }} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Mobile Number to Receive Text Messages' onChange={(e) => { setTextMessageNumber(e.target.value) }} variant={'outlined'} />
+          </Grid>
+          <Grid item xs={12}>
               <Button variant='contained' color='primary' onClick={submitForm}>Submit</Button>
-            </div>
           </Grid>
         </Grid>
       </Box>
