d9cf9dfb3b05286340a500f4d51f334e371090c4
Mac Deluca <Mac.Deluca@quartech.com>
2022-03-03 13:46:10 -0800

    moved strings to constants/strings

--- react/src/constants/strings.ts
+++ react/src/constants/strings.ts
@@ -203,7 +203,38 @@ const OnboardStrings = {
 const UserStrings = {
   deleteWarning: (username: string): string => `Are you sure you want to delete user ${username}?`,
 }
-
+const HomePageStrings = {
+  welcome: `Welcome to the BC Telemetry Warehouse (BCTW). The BCTW is an application and database to manage and store the
+          Province of British Columbia’s wildlife telemetry observations to support informed management decisions and
+          improve conservation efforts.`,
+  support: {
+    title: 'Need Support?',
+    data: [
+      {
+        textPrefix: 'For support, please contact ',
+        link: 'bctw@gov.bc.ca',
+        href: 'mailto:bctw@gov.bc.ca'
+      }
+    ]
+  },
+  resources: {
+    title: 'Other Resources',
+    data: [
+      {
+        textPrefix: 'Search for ',
+        link: 'wildlife data and information',
+        href: 'https://www.gov.bc.ca/wildlife-species-information'
+      },
+      {
+        textPrefix: 'Use the ',
+        textSuffix:
+          ' to access publicly available telemetry data layers derived from the BC Telemetry Warehouse and Wildlife Species Inventory (SPI) database for desktop GIS analyses.',
+        link: 'DataBC Data Catalogue',
+        href: 'https://catalogue.data.gov.bc.ca/dataset?q=wsi&download_audience=Public&sort=score+desc%2C+record_publish_date+desc'
+      }
+    ]
+  }
+};
 export {
   AnimalDelegationSteps,
   CodeStrings,
@@ -219,4 +250,5 @@ export {
   WorkflowStrings,
   UserAlertStrings,
   UserStrings,
+  HomePageStrings,
 }
--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Box, Grid, Link, Theme, Typography, CircularProgress } from '@mui/mater
 import makeStyles from '@mui/styles/makeStyles';
 import GovLinkBox from 'components/common/GovLinkBox';
 import { useTelemetryApi } from 'hooks/useTelemetryApi';
+import {HomePageStrings} from 'constants/strings';
 import { useEffect } from 'react';
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -17,39 +18,7 @@ const Home = (): JSX.Element => {
   const styles = useStyles();
   const api = useTelemetryApi();
   const { data, status, isLoading } = api.useCodeDesc('HOME_HDR');
-  const cards = {
-    welcome: `Welcome to the BC Telemetry Warehouse (BCTW). The BCTW is an application and database to manage and store the
-            Province of British Columbia’s wildlife telemetry observations to support informed management decisions and
-            improve conservation efforts.`,
-    support: {
-      title: 'Need Support?',
-      data: [
-        {
-          textPrefix: 'For support, please contact ',
-          link: 'bctw@gov.bc.ca',
-          href: 'mailto:bctw@gov.bc.ca'
-        }
-      ]
-    },
-    resources: {
-      title: 'Other Resources',
-      data: [
-        {
-          textPrefix: 'Search for ',
-          link: 'wildlife data and information',
-          href: 'https://www.gov.bc.ca/wildlife-species-information'
-        },
-        {
-          textPrefix: 'Use the ',
-          textSuffix:
-            ' to access publicly available telemetry data layers derived from the BC Telemetry Warehouse and Wildlife Species Inventory (SPI) database for desktop GIS analyses.',
-          link: 'DataBC Data Catalogue',
-          href: 'https://catalogue.data.gov.bc.ca/dataset?q=wsi&download_audience=Public&sort=score+desc%2C+record_publish_date+desc'
-        }
-      ]
-    }
-  };
-  const { welcome, support, resources } = cards;
+  const { welcome, support, resources } = HomePageStrings;
   return (
     <div className='container'>
         <Typography variant='h2' style={{ fontWeight: 'bold' }}>
