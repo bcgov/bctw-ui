7a627c61908724d3ccb47a918f16e8deec555caa
Mac Deluca <Mac.Deluca@quartech.com>
2022-03-07 12:04:48 -0800

    Added console.log for debugging

--- react/src/utils/errors.ts
+++ react/src/utils/errors.ts
@@ -4,13 +4,14 @@ import { AxiosError } from 'axios';
  * formats an Axios error to a string
  */
 const formatAxiosError = (err: AxiosError): string => {
-  let msg = err.message || '';
+  console.log(err)
+  let msg = err.message || 'An error occured';
   const {response} = err;
   const {data} = response;
   if(typeof data == 'string'){
     msg = data;
   } 
-  console.log(msg)
+  console.log(err)
   return `${msg}`;
 };
 
