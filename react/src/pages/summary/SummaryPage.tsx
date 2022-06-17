5eeca7f1310fc6e145c435ec869e537b2ab0ad80
Mac Deluca <Mac.Deluca@quartech.com>
2022-06-17 14:58:26 -0700

    Added linebreak to summary page

--- react/src/pages/summary/SummaryPage.tsx
+++ react/src/pages/summary/SummaryPage.tsx
@@ -1,13 +1,14 @@
-import MetabaseDashboard from 'components/common/MetabaseDashboard';
-import { DashboardItem } from 'types/metabase';
+import MetabaseDashboard, { DashboardItem } from 'components/common/MetabaseDashboard';
+
 
 export default function SummaryPage(): JSX.Element {
-  const dashboards: DashboardItem[] = [
-    { name: 'All Species', id: 3, icon: 'animals' },
-    { name: 'Caribou', id: 7, icon: 'animals' },
-    { name: 'Wolves', id: 9, icon: 'animals' },
-    { name: 'Moose', id: 8, icon: 'animals' },
-    { name: 'Bears', id: null, icon: 'animals' }
+  const dashboards:DashboardItem[] = [
+    { name: 'All Species',  id: 3,    icon: 'animals' },
+    { name: 'Caribou',      id: 7,    icon: 'animals' },
+    { name: 'Wolves',       id: 9,    icon: 'animals' },
+    { name: 'Moose',        id: 8,    icon: 'animals' },
+    { name: 'Bears',        id: null, icon: 'animals' },
+    { name: 'Data Quality', id: null, icon: 'database'}
   ]
-  return <MetabaseDashboard dashboardItems={dashboards}/>
+  return <MetabaseDashboard dashboardItems={dashboards} lineBreaks={[4]}/>
 }
--- react/src/types/metabase.ts
+++
@@ -1,9 +0,0 @@
-type DashboardItem = {
-  name: string;
-  id: number | null;
-  icon: string;
-}
-
-export type {
-  DashboardItem,
-}
