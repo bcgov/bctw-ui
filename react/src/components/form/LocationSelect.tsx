3bab384b21398a913d9d57a33d81b6849a210034
Graham Stewart <graham.stewart@quartech.com>
2022-10-19 16:59:29 -0700

    Fully decouples LocationSelect from ExportV2 and updates a bunch of styling / removes inline styling

--- react/src/components/form/LocationSelect.tsx
+++ react/src/components/form/LocationSelect.tsx
@@ -1,56 +1,119 @@
 import * as L from 'leaflet';
-import { FeatureLayer } from 'esri-leaflet';
-import LabeledMarker from 'leaflet-labeled-circle';
 import { Box, Paper } from '@mui/material';
 import TextField from 'components/form/TextInput';
 import { InboundObj } from 'types/form_types';
 import FileInput from './FileInput';
 import makeStyles from '@mui/styles/makeStyles';
 import { initMap } from 'pages/map/map_init';
+import shpjs from 'shpjs';
+import { FeatureCollection } from 'geojson';
+import { AllGeoJSON } from '@turf/helpers';
+import simplify from '@turf/simplify';
+import { useEffect, useRef, useState } from 'react';
 
 type ILocationSelectProps = {
-    defaultValues: string[];
-    map: React.MutableRefObject<L.Map>;
-    drawnItems: L.FeatureGroup;
-    handleDrawShape: () => void;
-    handleChange: (index: number, formObj: InboundObj) => void;
-    handleFile: (fieldName: string, files: FileList) => void;
+    handleDrawShape: (features: L.FeatureGroup) => void;
+    height?: string;
 }
 
-export const exportPageStyles = makeStyles(() => ({
-    containerDiv: {
-        height: '100px',
-        width: '100px'
+export const locationStyles = makeStyles((theme) => ({
+    paper: {
+        padding: '30px',
+    },
+    mapContainerBox: {
+        marginTop: theme.spacing(2)
+    },
+    mapContainerDiv: {
+        flex: '1 1 auto', 
+        position: 'relative'
+    },
+    button : {
+        marginLeft: 'auto'
     }
 }));
 
 export default function LocationSelect (props: ILocationSelectProps) : JSX.Element {
-    const {handleChange, handleFile, defaultValues, map, drawnItems, handleDrawShape} = props;
+    const { handleDrawShape, height} = props;
+    const styles = locationStyles();
 
-    const updateComponent = (): void => {
-        const drawOptions: L.Control.DrawOptions = {
-            circle: false,
-            circlemarker: false,
-            marker: false,
-            polyline: false
+    const mapRef = useRef<L.Map>(null);
+    const drawnItemsRef = useRef<L.FeatureGroup>(null);
+    const manualLayerRef = useRef<L.Rectangle>(null);
+
+    const [boundingBox, setBoundingBox] = useState(['','','','']);
+    /*
+    * 0 - Min Lat, 1 -  Max Lat,  2 - Min Lon, 3-  Max Lon
+    */
+
+    useEffect(() => {
+        const updateComponent = (): void => {
+            if(!mapRef.current) {
+                if(drawnItemsRef.current == null) {
+                    drawnItemsRef.current = new L.FeatureGroup();
+                }
+                const drawOptions: L.Control.DrawOptions = {
+                    circle: false,
+                    circlemarker: false,
+                    marker: false,
+                    polyline: false
+                }
+                initMap(mapRef, drawnItemsRef.current, new L.GeoJSON, () => handleDrawShape(drawnItemsRef.current), () => {}, () => {}, drawOptions);
+            }
+        };
+        updateComponent();
+    })
+
+    const onFileUpload = (field: string, files: FileList) => {
+        const file = files[0];
+        file.arrayBuffer().then((buff) => {
+            shpjs.parseZip(buff).then((geo) => {
+                const simplified = simplify(geo as AllGeoJSON, {tolerance: 0.01, highQuality: true});
+                const geoLayerGroup = L.geoJSON(simplified as FeatureCollection);
+                geoLayerGroup.eachLayer(o => o.addTo(drawnItemsRef.current));
+                handleDrawShape(drawnItemsRef.current);
+            });
+        }); 
+    }
+
+    const handleBoundingBox = (idx: number, val: InboundObj) => {
+        if(val['coord'] !== undefined) {
+            const newArr = [...boundingBox.slice(0, idx), String(val['coord']) , ...boundingBox.slice(idx+1)];
+            const bb = newArr.map(o => o === '' ? NaN : Number(o));
+            setBoundingBox(newArr);
+            
+            if(bb.some(o => !Number.isNaN(o))) {
+                let [ minlat, maxlat, minlon, maxlon ] = bb;
+                minlat = (Number.isNaN(minlat)) ? -90 : minlat;
+                maxlat = (Number.isNaN(maxlat)) ?  90 : maxlat;
+                minlon = (Number.isNaN(minlon)) ? -180: minlon;
+                maxlon = (Number.isNaN(maxlon)) ?  180: maxlon;
+                const a = new L.Rectangle([[minlat, minlon], [maxlat, maxlon]]);
+
+                if (manualLayerRef.current == null || mapRef.current.hasLayer(manualLayerRef.current) == false) {
+                    manualLayerRef.current = a.addTo(drawnItemsRef.current);
+                }
+                manualLayerRef.current.setBounds([[minlat, minlon], [maxlat, maxlon]]);
+            }
+            else if (mapRef.current.hasLayer(manualLayerRef.current) == true) {
+                drawnItemsRef.current.removeLayer(manualLayerRef.current);
+            }
+
+            handleDrawShape(drawnItemsRef.current);
         }
-        initMap(map, drawnItems, new L.GeoJSON, handleDrawShape, () => {}, () => {}, drawOptions);
-    };
-
-    updateComponent();
+    }
 
     return (
-        <Paper style={{padding: '30px', marginBottom:'1rem'}} elevation={3}>
+        <Paper className={styles.paper} elevation={3}>
             <Box display='flex'>
-                <FileInput buttonText='Upload Shapefile' buttonVariant='outlined' accept={'.zip'} onFileChosen={handleFile}/>
-                <TextField style={{marginLeft: 'auto'}} label={'Min Latitude'} changeHandler={(o) => { handleChange(0, o) }} propName={'coord'} defaultValue={''+defaultValues[0]}/>
-                <TextField label={'Max Latitude'} changeHandler={(o) => { handleChange(1, o)}} propName={'coord'} defaultValue={''+defaultValues[1]} />
-                <TextField label={'Min Longitude'} changeHandler={(o) => { handleChange(2, o)}} propName={'coord'} defaultValue={''+defaultValues[2]} />
-                <TextField label={'Max Longitude'} changeHandler={(o) => { handleChange(3, o)}} propName={'coord'} defaultValue={''+defaultValues[3]} />
+                <FileInput buttonText='Upload Shapefile' buttonVariant='outlined' accept={'.zip'} onFileChosen={onFileUpload}/>
+                <TextField className={styles.button} label={'Min Latitude'} changeHandler={(o) => { handleBoundingBox(0, o) }} propName={'coord'} defaultValue={''+boundingBox[0]}/>
+                <TextField label={'Max Latitude'} changeHandler={(o) => { handleBoundingBox(1, o)}} propName={'coord'} defaultValue={''+boundingBox[1]} />
+                <TextField label={'Min Longitude'} changeHandler={(o) => { handleBoundingBox(2, o)}} propName={'coord'} defaultValue={''+boundingBox[2]} />
+                <TextField label={'Max Longitude'} changeHandler={(o) => { handleBoundingBox(3, o)}} propName={'coord'} defaultValue={''+boundingBox[3]} />
             </Box>
-            <Box marginTop={'1rem'} height={'500px'}>
-                <div style={{flex: '1 1 auto', position: 'relative'}}>
-                    <div style={{height: '500px'}} id = 'map'></div>
+            <Box className={styles.mapContainerBox} height={height ?? '500px'}>
+                <div className={styles.mapContainerDiv}>
+                    <div style={{height: height ?? '500px'}} id = 'map'></div>
                 </div>
             </Box>
         </Paper>
--- react/src/pages/data/bulk/ExportV2.tsx
+++ react/src/pages/data/bulk/ExportV2.tsx
@@ -10,7 +10,7 @@ import DataTable from 'components/table/DataTable';
 import { CritterStrings as CS, ExportStrings, MapStrings } from 'constants/strings';
 import { useTelemetryApi } from 'hooks/useTelemetryApi';
 import ManageLayout from 'pages/layouts/ManageLayout';
-import { SyntheticEvent, useEffect, useRef, useState } from 'react';
+import { SyntheticEvent, useEffect, useState } from 'react';
 import { AttachedAnimal } from 'types/animal';
 import { headerToColumn } from 'utils/common_helpers';
 import DateInput from 'components/form/Date';
@@ -18,19 +18,14 @@ import dayjs from 'dayjs';
 import { InboundObj, parseFormChangeResult } from 'types/form_types';
 import { Button  } from 'components/common';
 import Checkbox from 'components/form/Checkbox';
-import { Grid, Paper, Tab, Tabs, Button as MUIButton } from '@mui/material';
+import { Grid, Tab, Tabs, Button as MUIButton } from '@mui/material';
 import ExportDownloadModal from './ExportDownloadModal';
 import { InfoBanner } from 'components/common/Banner';
 import ContainerLayout from 'pages/layouts/ContainerLayout';
 import QueryBuilder, { IFormRowEntry } from 'components/form/QueryBuilder';
-import { initMap } from 'pages/map/map_init';
 import makeStyles from '@mui/styles/makeStyles';
-import TextField from 'components/form/TextInput';
-import { FeatureCollection, GeoJsonObject } from 'geojson';
-import FileInput from 'components/form/FileInput';
-import shpjs from 'shpjs';
-import simplify from '@turf/simplify';
-import { AllGeoJSON } from '@turf/helpers';
+import { FeatureCollection } from 'geojson';
+import LocationSelect from 'components/form/LocationSelect';
 
 export interface DateRange {
     start: dayjs.Dayjs,
@@ -42,20 +37,23 @@ export enum TabNames {
     advanced = 'Advanced Export'
 }
 
-export const exportPageStyles = makeStyles(() => ({
-    containerDiv: {
-        height: '100px',
-        width: '100px'
+export const exportPageStyles = makeStyles((theme) => ({
+    section : {
+        marginBottom: theme.spacing(2),
+        marginTop: theme.spacing(2)
+    },
+    innerSection: {
+        marginBottom: theme.spacing(4)
+    },
+    queryRegionBox: {
+        marginBottom: theme.spacing(4),
+        width: '800px'
     }
 }));
 
 export default function ExportPageV2 (): JSX.Element {
     const api = useTelemetryApi();
     const styles =  exportPageStyles();
-    
-    const mapRef = useRef<L.Map>(null);
-    const drawnItemsRef = useRef<L.FeatureGroup>(null);
-    const manualLayerRef = useRef<L.Rectangle>(null);
 
     const operators: string[] = ['Equals','Not Equals'];
     const columns: string[] = ['species', 'population_unit', 'wlh_id', 'animal_id', 'device_id', 'frequency'];
@@ -68,47 +66,10 @@ export default function ExportPageV2 (): JSX.Element {
     const [critterIDs, setCritterIDs] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [selectedLifetime, setSelectedLifetime] = useState(false);
-    
-
-    const [boundingBox, setBoundingBox] = useState(['','','','']);
     const [currentGeometry, setCurrentGeometry] = useState<string[]>([]);
-    /*
-    * 0 - Min Lat, 1 -  Max Lat,  2 - Min Lon, 3-  Max Lon
-    */
-    if(drawnItemsRef.current == null) {
-        drawnItemsRef.current = new L.FeatureGroup();
-    }
 
-    const handleBoundingBox = (idx: number, val: InboundObj) => {
-        if(val['coord'] !== undefined) {
-            const newArr = [...boundingBox.slice(0, idx), String(val['coord']) , ...boundingBox.slice(idx+1)];
-            const bb = newArr.map(o => o === '' ? NaN : Number(o));
-            setBoundingBox(newArr);
-            
-            if(bb.some(o => !Number.isNaN(o))) {
-                let [ minlat, maxlat, minlon, maxlon ] = bb;
-                minlat = (Number.isNaN(minlat)) ? -90 : minlat;
-                maxlat = (Number.isNaN(maxlat)) ?  90 : maxlat;
-                minlon = (Number.isNaN(minlon)) ? -180: minlon;
-                maxlon = (Number.isNaN(maxlon)) ?  180: maxlon;
-                const a = new L.Rectangle([[minlat, minlon], [maxlat, maxlon]]);
-
-                if (manualLayerRef.current == null || mapRef.current.hasLayer(manualLayerRef.current) == false) {
-                    manualLayerRef.current = a.addTo(drawnItemsRef.current);
-                }
-                manualLayerRef.current.setBounds([[minlat, minlon], [maxlat, maxlon]]);
-            }
-            else if (mapRef.current.hasLayer(manualLayerRef.current) == true) {
-                drawnItemsRef.current.removeLayer(manualLayerRef.current);
-            }
-
-            handleDrawShape();
-        }
-
-    }
-
-    const handleDrawShape = (): void => {
-        const clipper: FeatureCollection = drawnItemsRef.current.toGeoJSON() as FeatureCollection;
+    const handleDrawShape = (features: L.FeatureGroup): void => {
+        const clipper = features.toGeoJSON() as FeatureCollection;
         if(clipper?.features?.length) {
             const arr = [];
             clipper.features.forEach(o => {
@@ -123,23 +84,6 @@ export default function ExportPageV2 (): JSX.Element {
     }
 
     const {data: crittersData, isSuccess: critterSuccess} = api.useAssignedCritters(0);
-
-    useEffect(() => {
-        const updateComponent = (): void => {
-          if (isTab(TabNames.advanced)) {
-            const drawOptions: L.Control.DrawOptions = {
-                circle: false,
-                circlemarker: false,
-                marker: false,
-                polyline: false
-            }
-            initMap(mapRef, drawnItemsRef.current, new L.GeoJSON, handleDrawShape, () => {}, () => {}, drawOptions);
-          }
-          //tracksLayer.bringToBack();
-        };
-        console.log('updateComponent')
-        updateComponent();
-    }, [tab]);
 
     useEffect(() => {
         areFieldsFilled();
@@ -182,8 +126,6 @@ export default function ExportPageV2 (): JSX.Element {
 
     const handleColumnChange = (newval: string, idx: number): void => {
         const o = rows[idx];
-        //console.log(crittersData.length);
-        //console.log(crittersData.map(o => o[formFields.formatHeaderAsProp(newval)]).filter((v, i, a) => a.indexOf(v) === i));
         o.column = headerToColumn(newval);
         o.value = [];
         setRows([...rows.slice(0, idx), o , ...rows.slice(idx+1)]);
@@ -194,7 +136,7 @@ export default function ExportPageV2 (): JSX.Element {
         o.operator = newval;
         setRows([...rows.slice(0, idx), o , ...rows.slice(idx+1)]);
     }
-    
+
     const handleAutocompleteChange = (newVals, idx) => {
         const o = rows[idx];
         o.value = newVals.map(n => n.value);
@@ -205,148 +147,115 @@ export default function ExportPageV2 (): JSX.Element {
         setTab(newVal);
     }
 
-    const onFileUpload = (field: string, files: FileList) => {
-        const file = files[0];
-        file.arrayBuffer().then((buff) => {
-            shpjs.parseZip(buff).then((geo) => {
-                const geoJsonObj = geo as FeatureCollection;
-                //geoJsonObj.features = geoJsonObj.features.map(o => convex(o as AllGeoJSON));
-                //const simplified = convex(geo as AllGeoJSON);
-                const simplified = simplify(geo as AllGeoJSON, {tolerance: 0.01, highQuality: true});
-                const geoLayerGroup = L.geoJSON(simplified as FeatureCollection);
-                geoLayerGroup.eachLayer(o => o.addTo(drawnItemsRef.current));
-                handleDrawShape();
-            });
-        }); 
-    }
-
-    //{CreateFormField(formFields, wfFields.get(rows[idx].column), () => {})}
-    //<SelectCode codeHeader='species' defaultValue='' changeHandler={() => {}} required={false} propName={'species'}></SelectCode>
-    //{CreateFormField(rows[idx].formField, getFormFieldObjForColumn(rows[idx].column), (v) => {handleValueChange(v, idx)}, {handleChangeMultiple: (v) => handleDropdownChange(v, idx), required: false, multiple: true, style: {minWidth: '300px'}})}
     return(
     <ManageLayout>
-    <h1>Export My Animal Telemetry</h1>
-    <Box marginTop={'15px'}>
-        <InfoBanner text={ExportStrings.infoBannerMesgs} />
-    </Box>
-    <ContainerLayout>
-    <Tabs value={tab} className='tabs' onChange={handleChangeTab}>
-        <Tab label={"Quick Export"} value={TabNames.quick}/>
-        <Tab label={"Advanced Export"} value={TabNames.advanced} />
-    </Tabs>
-    <h2>Specify Date Range</h2>
-        <Grid marginBottom={'2rem'} container spacing={2}>
-            <Grid item sm={2}>
-                <DateInput
-                fullWidth
-                propName='tstart'
-                disabled={selectedLifetime}
-                label={MapStrings.startDateLabel}
-                defaultValue={dayjs(start)}
-                changeHandler={handleChangeDate}
-                //maxDate={dayjs(end)}
-                />
-            </Grid>
-            <Grid item sm={2}>
-                <DateInput
-                fullWidth
-                disabled={selectedLifetime}
-                propName='tend'
-                label={MapStrings.endDateLabel}
-                defaultValue={dayjs(end)}
-                changeHandler={handleChangeDate}
-                //maxDate={dayjs(end)}
-                />
-            </Grid>
-            <Grid item sm={2}>
-                <Checkbox
-                propName={"animalLifetime"}
-                label={"All Telemetry"}
-                initialValue={selectedLifetime}
-                changeHandler={() => setSelectedLifetime((o) => !o)}
-                />
-            </Grid>
-        </Grid>
-    {isTab(TabNames.quick) && (
-        <>
-        <h2>Select Animals</h2>
-        <Box mb={4}>
-            <DataTable
-            headers={AttachedAnimal.attachedCritterDisplayProps}
-            title={CS.assignedTableTitle}
-            onSelectMultiple={ handleDataTableSelect }
-            queryProps={{ query: api.useAssignedCritters }}
-            isMultiSelect
-            /*onSelect={handleSelect}
-            deleted={deleted}
-            updated={updated}*/
-            />
+        <h1>Export My Animal Telemetry</h1>
+        <Box className={styles.section} /*marginTop={'15px'}*/>
+            <InfoBanner text={ExportStrings.infoBannerMesgs} />
         </Box>
-        <Button     
-            className='form-buttons' 
-            disabled={!collarIDs.length}
-            onClick={() => {setShowModal(true)}}>
+        <ContainerLayout>
+            <Tabs value={tab} className='tabs' onChange={handleChangeTab}>
+                <Tab label={"Quick Export"} value={TabNames.quick}/>
+                <Tab label={"Advanced Export"} value={TabNames.advanced} />
+            </Tabs>
+            <Box className={styles.innerSection}>
+                <h2>Specify Date Range</h2>   
+                <Grid container spacing={2}>
+                    <Grid item sm={2}>
+                        <DateInput
+                        fullWidth
+                        propName='tstart'
+                        disabled={selectedLifetime}
+                        label={MapStrings.startDateLabel}
+                        defaultValue={dayjs(start)}
+                        changeHandler={handleChangeDate}
+                        />
+                    </Grid>
+                    <Grid item sm={2}>
+                        <DateInput
+                        fullWidth
+                        disabled={selectedLifetime}
+                        propName='tend'
+                        label={MapStrings.endDateLabel}
+                        defaultValue={dayjs(end)}
+                        changeHandler={handleChangeDate}
+                        />
+                    </Grid>
+                    <Grid item sm={2}>
+                        <Checkbox
+                        propName={"animalLifetime"}
+                        label={"All Telemetry"}
+                        initialValue={selectedLifetime}
+                        changeHandler={() => setSelectedLifetime((o) => !o)}
+                        />
+                    </Grid>
+                </Grid>
+            </Box>
+            {isTab(TabNames.quick) && (
+                <>
+                <Box className={styles.innerSection}>
+                    <h2>Select Animals</h2>
+                    <DataTable
+                    headers={AttachedAnimal.attachedCritterDisplayProps}
+                    title={CS.assignedTableTitle}
+                    onSelectMultiple={ handleDataTableSelect }
+                    queryProps={{ query: api.useAssignedCritters }}
+                    isMultiSelect
+                    />
+                </Box>
+                <Button    
+                    className='form-buttons' 
+                    disabled={!collarIDs.length}
+                    onClick={() => {setShowModal(true)}}>
+                        Export
+                </Button>
+                </>
+                )
+            }
+            {isTab(TabNames.advanced) && (
+            <>
+            <h2>Build Query</h2>
+            <Box className={styles.queryRegionBox}>
+                <QueryBuilder
+                    rows={rows}
+                    operators={operators}
+                    columns={columns}
+                    data={crittersData}
+                    handleColumnChange={handleColumnChange}
+                    handleOperatorChange={handleOperatorChange}
+                    handleValueChange={handleAutocompleteChange}
+                    handleRemoveRow={handleRemoveRow}
+                    handleAddRow={handleAddNewRow}
+                    handleExport={() => setShowModal(true)}
+                    disabled={!formsFilled}
+                />
+            </Box>
+            <Box className={styles.innerSection}>
+                <h2>Select Location</h2>
+                <LocationSelect handleDrawShape={handleDrawShape}/>
+            </Box>
+            <Button
+                disabled={!formsFilled} 
+                onClick={() => setShowModal(true)}>
                 Export
-        </Button>
-        </>
-        )
-    }
-    {isTab(TabNames.advanced) && (
-    <>
-    <h2>Build Query</h2>
-        <Box width={800}>
-            <QueryBuilder
-                rows={rows}
-                operators={operators}
-                columns={columns}
-                data={crittersData}
-                handleColumnChange={handleColumnChange}
-                handleOperatorChange={handleOperatorChange}
-                handleValueChange={handleAutocompleteChange}
-                handleRemoveRow={handleRemoveRow}
-                handleAddRow={handleAddNewRow}
-                handleExport={() => setShowModal(true)}
-                disabled={!formsFilled}
-            />
-        </Box>
-        <h2>Select Location</h2>
-        <Paper style={{padding: '30px', marginBottom:'1rem'}} elevation={3}>
-            <Box display='flex'>
-                <FileInput buttonText='Upload Shapefile' buttonVariant='outlined' accept={'.zip'} onFileChosen={onFileUpload}/>
-                <TextField style={{marginLeft: 'auto'}} label={'Min Latitude'} changeHandler={(o) => { handleBoundingBox(0, o) }} propName={'coord'} defaultValue={''+boundingBox[0]}/>
-                <TextField label={'Max Latitude'} changeHandler={(o) => { handleBoundingBox(1, o) }} propName={'coord'} defaultValue={''+boundingBox[1]} />
-                <TextField label={'Min Longitude'} changeHandler={(o) => { handleBoundingBox(2, o)}} propName={'coord'} defaultValue={''+boundingBox[2]} />
-                <TextField label={'Max Longitude'} changeHandler={(o) => { handleBoundingBox(3, o)}} propName={'coord'} defaultValue={''+boundingBox[3]} />
-            </Box>
-            <Box marginTop={'1rem'} height={'500px'}>
-                <div style={{flex: '1 1 auto', position: 'relative'}}>
-                    <div style={{height: '500px'}} id = 'map'></div>
-                </div>
-            </Box>
-        </Paper>
-        <Button
-            //className={styles.rightJustifyButton}
-            disabled={!formsFilled} 
-            onClick={() => setShowModal(true)}>
-            Export
-        </Button>
-        </>
-    )}
+            </Button>
+            </>
+            )}
 
-    <ExportDownloadModal 
-        exportType={tab} 
-        open={showModal} 
-        handleClose={() => {setShowModal(false)} } 
-        rowEntries={rows} 
-        critterIDs={critterIDs}
-        collarIDs={collarIDs}
-        postGISstrings={currentGeometry}
-        range={{
-            start: selectedLifetime ? dayjs('1970-01-01') : start,
-            end: selectedLifetime ? dayjs() : end
-        }}>
-    </ExportDownloadModal>
-    </ContainerLayout>
+            <ExportDownloadModal 
+                exportType={tab} 
+                open={showModal} 
+                handleClose={() => {setShowModal(false)} } 
+                rowEntries={rows} 
+                critterIDs={critterIDs}
+                collarIDs={collarIDs}
+                postGISstrings={currentGeometry}
+                range={{
+                    start: selectedLifetime ? dayjs('1970-01-01') : start,
+                    end: selectedLifetime ? dayjs() : end
+                }}>
+            </ExportDownloadModal>
+        </ContainerLayout>
     </ManageLayout>
     )
 }
