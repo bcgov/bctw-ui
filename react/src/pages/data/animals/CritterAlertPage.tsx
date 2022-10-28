cfdb0dbc2824ed8a2c1b2457ee4dba5f9e8edf32
Graham Stewart <graham.stewart@quartech.com>
2022-10-28 16:40:10 -0700

    Properly renders arrays of Alerts and pulls data from Mortality props

--- react/src/pages/data/animals/CritterAlertPage.tsx
+++ react/src/pages/data/animals/CritterAlertPage.tsx
@@ -1,28 +1,79 @@
-import { Paper } from "@mui/material";
+import { lighten, Paper, Typography } from "@mui/material";
 import { BaseProps } from "@mui/material/OverridableComponent"
 import { Banner } from "components/common/Banner";
-import { MortalityAlert } from "types/alert"
-
+import { TelemetryAlert, MortalityAlert, eAlertType } from "types/alert"
+import makeStyles from '@mui/styles/makeStyles';
+import dayjs from "dayjs";
 type CritterAlertProps = {
-    alerts: MortalityAlert[];
+    alerts: TelemetryAlert[];
 }
 
-const AlertCard = (): JSX.Element => {
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      width: '100%',
+      marginBottom: theme.spacing(2)
+    },
+    listItem: {
+      display: 'list-item',
+      padding: 2
+    },
+    info: {
+      alignItems: 'center',
+      flexWrap: 'wrap'
+    },
+    spacing: {
+      marginTop: theme.spacing(2)
+    },
+    amogus: {
+        color: lighten(theme.palette.error.light, 0.9)
+    }
+  }));
+
+const AlertCard = (tAlert: TelemetryAlert): JSX.Element => {
+    const styles = useStyles();
+    const alertFormat = (tAlert: TelemetryAlert): JSX.Element => {
+
+        return (
+            <>
+            <Typography> {tAlert.formatAlert}</Typography>
+            {tAlert instanceof MortalityAlert && (
+                <>
+                <Typography className={styles.spacing}>{`Device ID ${tAlert.device_id} has detected a potential mortality. `}</Typography>
+                <Typography className={styles.spacing}>{`Species: ${tAlert.species} Animal ID: ${tAlert.animal_id} Wildlife Health ID: ${tAlert.wlh_id}`}</Typography>
+                </>
+            )}
+           
+            </>
+        )
+    }
+
     return (
         <>
         <Banner
-            variant={'info'}
-            text={'Alert'}
+            variant={'error'}
+            text={alertFormat(tAlert)}
         />
-        </>
+        </>    
     )
 }
 
 export const CritterAlertPage = ({alerts}: CritterAlertProps): JSX.Element => {
+
+    const alert = new MortalityAlert;
+    alert.alert_type = eAlertType.mortality;
+    alert.wlh_id = '13-1444';
+    alert.species = 'Caribou';
+    alert.animal_id = '444233';
+    alert.device_id = 1234;
+    alert.valid_from = dayjs();
+
+    const arr = [alert, alert, alert];
+
     return (
         <>
         <h2>Alerts</h2>
-        <AlertCard/>
+        { arr.map(a => (AlertCard(a) )) }
         </>
     )
 }
