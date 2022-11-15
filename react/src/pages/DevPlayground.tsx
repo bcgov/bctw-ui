38d51f7571807896eb96972be362968246517271
Mac Deluca <Mac.Deluca@quartech.com>
2022-11-15 14:04:57 -0800

    Basic styling for keyx

--- react/src/pages/DevPlayground.tsx
+++ react/src/pages/DevPlayground.tsx
@@ -1,8 +1,21 @@
-import { Box, Button, ButtonBase, Card, CardContent, Grid, Link, List, ListItem, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  ButtonBase,
+  Card,
+  CardContent,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  Typography,
+  useTheme
+} from '@mui/material';
 import { InfoBanner } from 'components/alerts/Banner';
+import Icon from 'components/common/Icon';
 import { SubHeader } from 'components/common/partials/SubHeader';
 import { BannerStrings } from 'constants/strings';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Collar } from 'types/collar';
 import ManageLayout from './layouts/ManageLayout';
 // Place component constants / objects here
@@ -37,41 +50,82 @@ interface KeyXCardProps {
   deviceIds: number[];
 }
 
+interface KeyXPayload {
+  [device_id: number]: boolean;
+}
+
 // Temporarily build components down here for development
 const TempComponent = ({ deviceIds }: KeyXCardProps): JSX.Element => {
+  const theme = useTheme();
+  const [keyXPayload, setKeyXPayload] = useState<KeyXPayload>({});
+  useEffect(() => {
+    const tmp: KeyXPayload = {};
+    deviceIds.forEach((device_id) => {
+      //Temp stuff
+      if (device_id === DEVICE_IDS[0] || device_id === DEVICE_IDS[3]) {
+        tmp[device_id] = true;
+      }
+      setKeyXPayload(tmp);
+    });
+  }, [deviceIds]);
+  {
+    /* <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'></Box> */
+  }
   return (
     <>
       <Card sx={{ width: '24rem' }}>
         <CardContent sx={{ paddingBottom: 0 }}>
           <InfoBanner text={BannerStrings.vectronicKeyxInfo} />
         </CardContent>
-        <Box px={5} pb={2}>
-          <List>
+        <Box px={5} pb={2} display='flex' flexDirection='row' justifyContent='space-between'>
+          <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
             {deviceIds.map((id, i) => (
-              <ListItem key={`deviceid-${i}`}>
-                <Box display='flex' flexDirection='row' alignItems='center' justifyContent='space-between' width='100%'>
-                  <Box>
-                    {i == 0 && (
-                      <Typography pb={2} fontWeight='bold'>
-                        Device ID
-                      </Typography>
-                    )}
-                    {id}
-                  </Box>
-                  <Box>
-                    {i == 0 && (
-                      <Typography pb={2} fontWeight='bold'>
-                        KeyX File
-                      </Typography>
-                    )}
-                    <Link underline='hover'>Browse files</Link>
-                  </Box>
-                </Box>
-              </ListItem>
+              <>
+                {i == 0 && (
+                  <Typography pb={2} fontWeight='bold'>
+                    Device ID
+                  </Typography>
+                )}
+                <Typography>{id}</Typography>
+              </>
             ))}
-          </List>
+          </Box>
+          <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
+            {deviceIds.map((id, i) => (
+              <>
+                {i == 0 && (
+                  <Typography pb={2} fontWeight='bold'>
+                    KeyX file
+                  </Typography>
+                )}
+                {keyXPayload[id] ? (
+                  <Icon icon={'check'} htmlColor={theme.palette.success.main} />
+                ) : (
+                  <Link underline='hover'>Browse files</Link>
+                )}
+              </>
+            ))}
+          </Box>
         </Box>
       </Card>
     </>
   );
 };
+
+{
+  /* <Box>
+  {i == 0 && (
+    <Typography pb={2} fontWeight='bold'>
+    KeyX File
+    </Typography>
+    )}
+    {keyXPayload[id] ? (
+      <Icon icon={'check'} htmlColor={theme.palette.success.main} />
+      ) : (
+        <Link underline='hover'>Browse files</Link>
+        )}
+      </Box> */
+}
+{
+  /* </Box> */
+}
