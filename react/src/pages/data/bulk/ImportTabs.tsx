fca09448825b6a8df4c02c0dc819bec23210169b
Graham Stewart <graham.stewart@quartech.com>
2022-12-12 11:46:24 -0800

    Changed download tag to include filename

--- react/src/pages/data/bulk/ImportTabs.tsx
+++ react/src/pages/data/bulk/ImportTabs.tsx
@@ -172,7 +172,7 @@ export const ImportAndPreviewTab = (props: ImportTabProps & { sheetIndex: SheetN
             <SubHeader text={`${title} Import`} />
           </Box>
           <Button
-            download
+            download={constants.templateFileName}
             href={createUrl({ api: 'get-template', query: 'file_key=import_template' })}
             style={{ marginLeft: 'auto' }}
             variant='outlined'>
